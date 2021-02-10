70c0ab033851dfe7f91496bbeb705526aa0244c0
Nate Bate <nate@harvestmedia.com>
2021-02-10 14:52:56 -0500

    update to setup.sh and .gitignore

--- setup.sh
+++ setup.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+# Check to make sure mysql is installed and available
+if ! [ -x "$(command -v mysql)" ]; then
+  echo 'Error: mysql is not installed and/or not in your PATH.' >&2
+  exit 1
+fi
+
+# Check to make sure valet is installed and available
+if ! [ -x "$(command -v valet)" ]; then
+  echo 'Error: valet is not installed and/or not in your PATH.' >&2
+  exit 1
+fi
+
+# Make sure this isn't the project repo. This file should not be run in the project repo.
+FILE=.project.repo
+if test -f "$FILE"; then
+    printf "\n$FILE exists.  Exiting.  This should not be run in the project repo.\n\n"
+fi
+
+# Make sure the first parameter is included
 if [ $# -eq 0 ]
   then
     printf "\n\nPlease include the desired database name as the first parameter. Exiting.\n\n"
     exit 1
 fi
 
+# make sure the mysql config file exists.  If not, create it.
 FILE=~/.my.cnf
 if test -f "$FILE"; then
     printf "\n$FILE exists.  Proceeding.\n\n"
@@ -47,3 +67,4 @@ printf "2. Run 'valet link'\n"
 printf "3. Update the environment URLs in the .env file.\n"
 printf "4. Log into the craftcms CP. \n\tThe Default Username is dev@harvestmedia.com
 \n\tThe Default password is CoffeeIsAwesome\n\n"
+
